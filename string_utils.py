f58be06294289d3cac9387c815ab0b33bc42c7ea
Tzvi Sender <gr58cw5c59@privaterelay.appleid.com>
2025-11-17 14:50:25 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,34 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    split_formula = []
+    if not formula:
+        return split_formula
 
+    start_index = 0
+    for i in range(1, len(formula)):
+        if formula[i].isupper():
+            split_formula.append(formula[start_index:i])
+            start_index = i
+
+    split_formula.append(formula[start_index:])
+    return split_formula
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1 
+
+    
+    for i in range(1, len(formula)):
+        if formula[i].isdigit():
+          
+            digit_location = i
+            break 
+        else:
+          
+            digit_location += 1
+
+  
+    if digit_location == len(formula):
+        return formula, 1
+    else:
+        prefix = formula[:digit_location]
+        numeric_part = int(formula[digit_location:])
+        return prefix, numeric_part
